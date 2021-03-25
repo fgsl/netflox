277288844c6d003fdd31eb9b760354b88ddde7d5
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2021-03-24 22:42:13 -0300

    Prepara módulos para serem convertidos para Mezzio

---
+++ homepage/src/App/src/Handler/AboutHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Laminas\Diactoros\Response\HtmlResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Mezzio\Template\TemplateRendererInterface;
+
+use function time;
+
+class AboutHandler implements RequestHandlerInterface
+{
+    /** @var TemplateRendererInterface */
+    private $template;
+    
+    public function __construct(TemplateRendererInterface $template)
+    {
+        $this->template = $template;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new HtmlResponse($this->template->render('app::about',[]));
+    }
+}
